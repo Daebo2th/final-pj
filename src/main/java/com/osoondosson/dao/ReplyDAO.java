baac6ddf31302ad10db7b05aa53928e05214678c
uhzoo0916 <uhzoo3366@gmail.com>
2023-09-14 20:20:17 +0900

    <Feat: 댓ê¸ 기능 추가(교직원 / 교육생)

--- src/main/java/com/osoondosson/dao/ReplyDAO.java
+++ src/main/java/com/osoondosson/dao/ReplyDAO.java
@@ -1,4 +1,12 @@
 package com.osoondosson.dao;
 
+import com.osoondosson.vo.ReplyVO;
+import com.osoondosson.vo.TaskVO;
+
+import java.util.List;
+
 public interface ReplyDAO {
+    void insertReply(ReplyVO vo);
+
+    List<TaskVO> getReplyList(int taskSeq);
 }
