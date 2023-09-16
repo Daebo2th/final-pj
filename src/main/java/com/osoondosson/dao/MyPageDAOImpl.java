885e10654b706c30f2ead46b5c7362f766e7401e
hyeokjin0317 <hyukjin0317@naver.com>
2023-09-16 17:50:15 +0900

    add password update

--- src/main/java/com/osoondosson/dao/MyPageDAOImpl.java
+++ src/main/java/com/osoondosson/dao/MyPageDAOImpl.java
@@ -1,5 +1,7 @@
 package com.osoondosson.dao;
 
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -22,6 +24,18 @@ public class MyPageDAOImpl implements MyPageDAO {
 	public void updateMyPage(UserVO vo) {
 		System.out.println("==> mybatis updateMyPage() 호출 ");
 		mybatis.update("com.osoondosson.dao.MyPageDAO.updateMyPage", vo);
+	}
+
+	@Override
+	public String selectPassWord(String userId) {
+		System.out.println("==> mybatis selectPassWord() 호출 ");
+		return mybatis.selectOne("com.osoondosson.dao.MyPageDAO.selectPassWord", userId);
+	}
+
+	@Override
+	public int updatePassWord(Map<String, Object> paramMap) {
+		System.out.println("==> mybatis updatePassWord() 호출 ");
+		return mybatis.update("com.osoondosson.dao.MyPageDAO.updatePassWord", paramMap);
 	}
 	
 	
